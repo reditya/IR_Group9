75d5d9279d5207939d18916307ff14814cdfb855
Laurens van den Bercken <laurensvdbercken@gmail.com>
2017-03-11 11:40:09 +0100

    Get all food categories

--- get_foodplaces_adam.py
+++ get_foodplaces_adam.py
@@ -1,19 +1,33 @@
 import requests
 
 api = 'https://api.foursquare.com'
-endpoint = '/v2/venues/search'
+cat_endpoint = '/v2/venues/categories'
+search_endpoint = '/v2/venues/search'
 
-# Mandatory parameters
+# Mandatory api parameters
 params = {
 	'client_id' : 'D1YBOR4SMZE2PUVRXXOCU1MKSIKKN01A4PCKNMTGEYZGELN1',
 	'client_secret' : 'DPK3AIXZHLTP4MMFSNMPSZQEWHJLGXXXFCOLKVVDKIVLYLAW',
 	'v' : '20170311'
 }
 
+cat_request = requests.get(api+cat_endpoint, params=params)
+cat_response = cat_request.json()
+categories = cat_response['response']['categories']
+food_categories = []
+
+for category in categories:
+	if(category['name'] == 'Food'):
+		food_categories = category['categories']
+
+print(len(food_categories))
+
 params['near'] = 'Amsterdam'
+# max = 50
+params['limit'] = 50
 # Category = food
 params['categoryId'] = '4d4b7105d754a06374d81259'
 
-request = requests.get(api+endpoint, params=params)
-response = request.json()
-print(response['response']['venues'])
+search_request = requests.get(api+search_endpoint, params=params)
+search_response = search_request.json()
+print(len(search_response['response']['venues']))
