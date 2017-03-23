d0ff6b9b1a196426f24687be0fa0dc296b189c3f
Reza Permadi <reditya@gmail.com>
2017-03-23 14:08:38 +0100

    revise instagram post

--- instagram/parse_json.py
+++ instagram/parse_json.py
@@ -15,10 +15,10 @@ with open(coordinate_file) as f:
 	for l in f:
 		d = l.split("|")
 		loc = {}
-		loc['id'] = d[0]
-		loc['name'] = d[1]
-		loc['lat'] = d[2]
-		loc['lng'] = d[3]
+		#loc['id'] = d[0]
+		#loc['name'] = d[1]
+		loc['lat'] = d[2].rstrip()
+		loc['lng'] = d[3].rstrip()
 		location_dict[d[0]] = loc
 
 # read the instagram posts file
@@ -29,7 +29,9 @@ with open(post_file) as f:
     		if d['locationId'] is not None and d['caption'] is not None:
 				post['createdTime'] = d['createdTime']
 				post['text'] = d['caption'].replace('\r', ' ').replace('\n', ' ')
-				post['location'] = location_dict[d['locationId']]
+				post['locationId'] = d['locationId']
+				post['locationName'] = d['locationName']
+				post['coordinate'] = location_dict[d['locationId']]
 				#print str(d['createdTime']) + "" + d['caption'].replace('\r', ' ').replace('\n',' ').encode('utf8') + "|" + str(d['locationId'])
 				json.dump(post, outfile)
 				outfile.write("\n")
