41ffdd93d94e2dc91f971a2829839a5c922319cd
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-03-28 23:00:30 +0200

    Script for querying data in ES from PHP

--- gui/index.php
+++ gui/index.php
@@ -1,5 +1,40 @@
 <?php
 
+require 'vendor/autoload.php';
+
+$hosts = [
+    // This is effectively equal to: "https://username:password!#$?*abc@foo.com:9200/"
+    // [
+    //     'host' => 'foo.com',
+    //     'port' => '9200',
+    //     'scheme' => 'https',
+    //     'user' => 'username',
+    //     'pass' => 'password!#$?*abc'
+    // ],
+
+    // This is equal to "http://localhost:9200/"
+    [
+        'host' => 'localhost',    // Only host is required
+    ]
+];
+
+$client = ClientBuilder::create()           // Instantiate a new ClientBuilder
+                    ->setHosts($hosts)      // Set the hosts
+                    ->build();              // Build the client object
+
+$client = Elasticsearch\ClientBuilder::create()->build();
+
+$searchParams = "restaurants/_search";
+
+try {
+    $client->search($searchParams);
+} catch (Elasticsearch\Common\Exceptions\TransportException $e) {
+    $previous = $e->getPrevious();
+    if ($previous instanceof 'Elasticsearch\Common\Exceptions\MaxRetriesException') {
+        echo "Max retries!";
+    }
+}
+
 echo "Test GUI";
 
 ?>
