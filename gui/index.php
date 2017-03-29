c8465818e90df8e17138b6dc8f8af548a5c8d56f
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-03-29 20:54:23 +0200

    Fix index.php in GUI

--- gui/index.php
+++ gui/index.php
@@ -2,53 +2,6 @@
 
 require 'vendor/autoload.php';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$client = ClientBuilder::create()           // Instantiate a new ClientBuilder
-                    ->setHosts($hosts)      // Set the hosts
-                    ->build();              // Build the client object
-
-$client = Elasticsearch\ClientBuilder::create()->build();
-
-$searchParams = "restaurants/_search";
-
-try {
-    $client->search($searchParams);
-} catch (Elasticsearch\Common\Exceptions\TransportException $e) {
-    $previous = $e->getPrevious();
-    if ($previous instanceof 'Elasticsearch\Common\Exceptions\MaxRetriesException') {
-        echo "Max retries!";
-    }
-}*/
-
 use Elasticsearch\ClientBuilder;
 
 $client = Elasticsearch\ClientBuilder::create()
