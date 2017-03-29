9bf9b77d19cc3d0c7e49fa9c8b712214236c2857
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-03-29 23:06:42 +0200

    Add feedback widget to GUI

--- gui/index.php
+++ gui/index.php
@@ -21,6 +21,43 @@ $params = [
 
 $results = $client->search($params);
 
-echo $results['took'];
+//echo $results['took'];
 
 ?>
+
+<html>
+<head>
+    <title>A Leaflet map!</title>
+    <link rel="stylesheet" href="leaflet/leaflet.css" />
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/feedback.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="leaflet/leaflet.js"></script> 
+    <script src="jquery-2.1.1.min.js"></script>
+    <script src='https://npmcdn.com/@turf/turf/turf.min.js'></script>  
+    <script src='https://api.mapbox.com/mapbox.js/plugins/turf/v2.0.2/turf.min.js'></script> 
+</head>
+<body>
+    <div class="container">
+        <div id="feedback">
+            <div id="feedback-form" style='display:none;' class="col-xs-4 col-md-4 panel panel-default">
+                <p class="feedback"><img src="images/thumb.png" height="10%"> our app?</p>
+                <form method="POST" action="/feedback" class="form panel-body" role="form">
+                    <label>
+                        <input type="radio" name="optradio"> Yes
+                    </label>
+                    <label>
+                        <input type="radio" name="optradio"> No
+                    </label>
+                </form>
+            </div>
+            <div id="feedback-tab">Feedback</div>
+        </div>
+        <div class="map" id="map"></div>
+    </div>
+
+    <script src="javascript/add_data.js"></script>
+    <script src="http://code.jquery.com/jquery-1.12.3.min.js"></script>
+    <script src="javascript/feedback.js"></script>
+</body>
+</html>
