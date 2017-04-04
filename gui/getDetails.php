dad65daf0cd5324f55e7b72945f513e809d23ff4
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-04-04 13:13:02 +0200

    Add ajax to invoke the ES function

---
+++ gui/getDetails.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+Parameters are: lon, lat, size and range
+Return JSON
+*/
+
+require 'vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+header('Content-type: application/json');
+
+if(isset($_GET['range']) && isset($_GET['lon']) && isset($_GET['lat']) && isset($_GET['size'])) {
+	
+	$client = Elasticsearch\ClientBuilder::create()
+	    ->setHosts(["54.171.151.130:9200"])
+	    ->setRetries(0)
+	    ->build();
+
+	$params = [
+	    'index' => 'restaurants',
+	    'type' => 'restaurant',
+	    'body' => [
+		'size' => $_GET['size'],
+		'query' => [
+		  'match_all' => new \stdClass()
+		],
+		'filter' => [
+		  'geo_distance' => [
+			'distance' => $_GET['range'],
+			'location' => [
+				'lat' => $_GET['lat'],
+				'lon' => $_GET['lon']
+			]
+		  ]
+		]
+	    ]
+	];
+
+	$results = $client->search($params);
+
+	return json_encode($results);
+	
+} else {
+	$return = [
+    		'error' => 'Parameter missing.',
+	];
+
+	return json_encode($return);
+}
+?>
--- gui/javascript/handler.js
+++ gui/javascript/handler.js
@@ -3,7 +3,23 @@ var popup = L.popup();
 function onMapClick(e) {
   popup
       .setLatLng(e.latlng)
-      .setContent("You clicked the map at " + e.latlng.toString())
+      .setContent(
+      	$.ajax(
+	        {
+	        	url:"index.php/a", 
+	        	type:"POST",
+	        	contentType:"application/json; charset=utf-8",
+	                    data:{some_string:"resturants"},
+	                    dataType:"json",
+	                    success:function(data){
+	                    	"Restaurants" + data;
+	                    },
+	                    error:function(a,b,c){
+
+	                    }
+	        }
+	    )
+      	"Restaurants " + e.latlng.toString())
       .openOn(map);
 }
 
