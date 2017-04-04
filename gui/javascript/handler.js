d827ea00638d3cfff2e4077619d512b95e6d45a6
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-04-04 23:39:22 +0200

    Fix error

--- gui/javascript/handler.js
+++ gui/javascript/handler.js
@@ -10,6 +10,7 @@ function onMapClick(e) {
 function fetchData(){
 	var query = 'getDetails.php?range=1km&lat=52.379189&lon=4.899431&size=5';
 	$.getJSON(query,function(data){
+		alert("A");
 		return "Success";
 	});
 
