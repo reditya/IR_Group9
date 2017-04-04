fb33b8c445fc6d895f8573d17d11ab9ddc4b1a81
andreasmcp <andreas.mc.pangaribuan@gmail.com>
2017-04-04 22:46:13 +0200

    Trial and error fetch data

--- gui/javascript/handler.js
+++ gui/javascript/handler.js
@@ -3,20 +3,23 @@ var popup = L.popup();
 function onMapClick(e) {
   popup
       .setLatLng(e.latlng)
-      .setContent(
-      	$.ajax(
-	        {
-	        	url:"getDetails.php?range=1km&lat=52.379189&lon=4.899431&size=5", 
-	        	type:"POST",
-	        	contentType:"application/json; charset=utf-8",
-	                    data:{some_string:"resturants"},
-	                    dataType:"json",
-	                    success:function(data){
-	                    	alert(data);
-	                    }
-	        }
-	    ))
+      .setContent(fetchData())
       .openOn(map);
+}
+
+function fetchData(){
+	$.ajax(
+		{
+			url:"getDetails.php?range=1km&lat=52.379189&lon=4.899431&size=5", 
+			type:"POST",
+			contentType:"application/json; charset=utf-8",
+			data:{some_string:"resturants"},
+			dataType:"json",
+			success:function(data){
+				return "success";
+			}
+		}
+	)
 }
 
 map.on('click', onMapClick);
