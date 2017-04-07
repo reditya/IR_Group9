d01f2d88f9e53bf4d454bb08e093e938decec5c4
aggie <agathe.balayn@ensta-paristech.fr>
2017-04-07 19:03:14 +0200

    added category clustring

---
+++ map_test/script_list/javascript/add_data.js
@@ -0,0 +1,275 @@
+  // initialize the map
+  var southWest = L.latLng(52.29,4.73);
+  var northEast = L.latLng(52.42,4.98);
+  var bounds = L.latLngBounds(southWest, northEast)
+  var map = L.map('map', {
+    maxBounds: bounds
+  }).setView([52.3702, 4.8952], 13);
+
+
+  var geojsonMarkerOptions = {
+      radius: 8,
+      fillColor: "#ff7800",
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+
+  
+  var data_list = []
+
+  function add_base_map(){
+    L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      subdomains: 'abcd',
+      minZoom: 10,
+      maxZoom: 20,
+      ext: 'png'
+    }).addTo(map);
+  }
+
+  
+  function onEachFeature_insta(feature, layer) {
+    if (feature.properties.category.length != 0){
+    var food = feature.properties.category;
+    var list = '<ul class="myList"><li><a>' + food.join('</a></li><li>') + '</li></ul>';
+    layer.bindPopup(list);
+  }
+  }
+
+  function onEachFeature_fours(feature, layer) {
+    
+    if (feature.properties.category) {
+      layer.bindPopup(feature.properties.category);
+    }   
+  }
+  function onEachFeature_poly(feature, layer) {
+    
+    if (feature.properties.category) {
+      layer.bindPopup(feature.properties.category);
+    }   
+  }
+
+  function map_points_fours(data){
+    // Points
+      L.geoJson(data, {
+      pointToLayer: function (feature, latlng) {   
+      
+        geojsonMarkerOptions.fillColor = color_clust[Math.floor(feature.properties.cluster)];
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+      },
+          onEachFeature: onEachFeature_fours 
+    }).addTo(map);
+  }
+
+  function map_points_insta(data){
+    // Points
+      L.geoJson(data, {
+      pointToLayer: function (feature, latlng) { 
+      data_list.push([latlng.lat, latlng.lng,feature.properties.intensity])  
+        if (feature.properties.category.length != 0){
+          geojsonMarkerOptions.fillColor = color_clust[Math.floor(feature.properties.cluster)];
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+        
+      },
+          onEachFeature: onEachFeature_insta 
+    }).addTo(map);
+      //console.log(data_list)
+  }
+
+
+  function prepare_cluster_color(data){
+    // Find number of clusters and create array of points with in clusters
+    nb_clust = data['features'].map(function(value, index) {return value['properties']['cluster']});
+    bool_food_list = []
+    var n = []; 
+    for(var i = 0; i < nb_clust.length; i++) 
+    {
+      bool_food_list.push([])
+      if (n.indexOf(nb_clust[i]) == -1) n.push(nb_clust[i]);
+    }
+    nb_clust = n.length
+    //console.log(n)
+    for(var i = 0; i < data['features'].length; i++) {
+      //console.log(data['features'][i]['properties'])
+      /*
+      if (data['features'][i]['properties']['category'].length >0){
+      console.log(data['features'][i]['properties']['category'].length )
+          console.log(data['features'][i]['properties']['category'])
+}*/
+      if (data['features'][i]['properties']['category'].length != 0){
+        bool_food_list[ Math.floor(data['features'][i]['properties']['cluster'])] = 1
+      }
+    } 
+    //console.log(nb_clust);
+    color_clust = [];
+    // Associate random colors to clusters
+    for (i = 0; i < nb_clust; i++) {
+      // Random color '#'+Math.floor(Math.random()*16777215).toString(16);
+      if (bool_food_list[i] > 0){
+              color_clust.push('#'+Math.floor(Math.random()*16777215).toString(16));
+      }
+      else
+      {
+              color_clust.push('no');
+      }
+
+    }
+    
+
+    
+  }
+
+  function show_cluster(data){
+    cluster_list = new Array(nb_clust);
+
+    for (i = 0; i < nb_clust; i++) {
+      // Random color '#'+Math.floor(Math.random()*16777215).toString(16);
+      cluster_list[i] = new Array;
+    }
+    console.log(nb_clust);
+    // var hull = turf.convex(data);
+    //L.geoJson(hull).addTo(map);
+    //  L.polygon(hull).addTo(map);  
+     
+    L.geoJson(data, {
+      filter: function (feature, layer) {  
+        //console.log(feature.geometry.coordinates[0][0][0])
+        //if (Math.floor(feature.properties.cluster) === 1) {
+        //          console.log(Math.floor(feature.properties.cluster))
+        //}
+        cluster_list[Math.floor(feature.properties.cluster)].push([feature.geometry.coordinates[0][0][0][0],feature.geometry.coordinates[0][0][0][1]],[feature.geometry.coordinates[0][0][1][0],feature.geometry.coordinates[0][0][0][1]],[feature.geometry.coordinates[0][0][1][0],feature.geometry.coordinates[0][0][1][1]],[feature.geometry.coordinates[0][0][0][0],feature.geometry.coordinates[0][0][1][1]]);
+      }
+    })
+  
+    
+    collection_cluster = new Array(nb_clust);
+    for (i = 0; i < nb_clust; i++) {
+      test = [];
+      for (d in cluster_list[i]){
+          test.push(turf.point(cluster_list[i][d]))
+      }
+      collection_cluster[i] = turf.featurecollection(test)
+    }
+     // Polygons
+    for (d in collection_cluster){      
+      //console.log((collection_cluster[d]))
+     // var hull = turf.convex(collection_cluster[d]);   
+               if (color_clust[d] != 'no'){
+
+      L.geoJson(turf.convex(collection_cluster[d]),{
+        onEachFeature: onEachFeature_poly,
+        style: function(feature) {
+                      return {color: color_clust[d]};       
+        }
+
+      }).addTo(map);
+    }
+    }
+    
+  }
+
+  function show_cluster_max(data){
+    cluster_list = new Array(nb_clust);
+    for (i = 0; i < nb_clust; i++) {
+      // Random color '#'+Math.floor(Math.random()*16777215).toString(16);
+      cluster_list[i] = new Array;
+    }
+    // var hull = turf.convex(data);
+    //L.geoJson(hull).addTo(map);
+    //  L.polygon(hull).addTo(map);  
+     
+    L.geoJson(data, {
+      filter: function (feature, layer) {  
+        //console.log(feature.geometry.coordinates[0][0][0])
+        cluster_list[Math.floor(feature.properties.cluster)].push([feature.geometry.coordinates[0][0][0][0],feature.geometry.coordinates[0][0][0][1]],[feature.geometry.coordinates[0][0][1][0],feature.geometry.coordinates[0][0][0][1]],[feature.geometry.coordinates[0][0][1][0],feature.geometry.coordinates[0][0][1][1]],[feature.geometry.coordinates[0][0][0][0],feature.geometry.coordinates[0][0][1][1]]);
+      }
+    })
+  
+    
+    collection_cluster = new Array(nb_clust);
+    for (i = 0; i < nb_clust; i++) {
+      test = [];
+      for (d in cluster_list[i]){
+          test.push(turf.point(cluster_list[i][d]))
+      }
+      collection_cluster[i] = turf.featurecollection(test)
+    }
+     // Polygons
+    for (d in collection_cluster){      
+      //console.log((collection_cluster[d]))
+     // var hull = turf.convex(collection_cluster[d]);     
+      L.geoJson(turf.convex(collection_cluster[d]),{
+        onEachFeature: onEachFeature_poly,
+        style: function(feature) {
+          return {color: color_clust[d]};       
+        }
+      }).addTo(map);
+    }
+    
+  }
+  
+
+  function show_heatmap(data){
+    /*
+    L.geoJson(data, {
+      pointToLayer: function (feature, latlng) {   
+        if (feature.properties.category.length != 0){
+          geojsonMarkerOptions.fillColor = color_clust[Math.floor(feature.properties.cluster)];
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+        
+      },
+          onEachFeature: onEachFeature_insta 
+    }).addTo(map);
+  */
+    var heat = L.heatLayer(data_list, {radius: 30, maxZoom: 15}).addTo(map);
+  }
+
+
+/*
+// Heatmap
+  // For one specific food term
+  $.getJSON("new_point.geojson",function(data){
+    add_base_map();
+    prepare_cluster_color(data);
+    map_points_insta(data);
+    show_heatmap(data)
+
+    //show_cluster(data);          
+
+  })
+*/
+
+  // Instagram data    
+  
+  
+  // All categories
+  $.getJSON("new_point.geojson",function(data){
+    add_base_map();
+    prepare_cluster_color(data);
+    //show_cluster(data);          
+
+  })
+  $.getJSON("new_poly.geojson",function(data){
+    show_cluster(data);          
+        
+  })
+
+    $.getJSON("new_point.geojson",function(data){
+          map_points_insta(data);
+
+})
+
+  
+  // Foursquare data
+  /*
+  $.getJSON("all_resto_geojson_k2_f1.geojson",function(data){
+    add_base_map();
+    show_cluster(data);          
+    map_points_fours(data);
+
+  })
+  */
---
+++ map_test/script_list/show_cat.py
@@ -0,0 +1,64 @@
+from sys import argv
+from os.path import exists
+script, in_file1, in_file2, out_file, out_file_2 , cat_file, category, bool_heatmap = argv
+from list_function import *
+
+def show_all_cat(cat_file, out_file, out_file_2):
+	nb_data_1, nb_type_1, ref_list_1, coor_1, weight_1 = pre_info_tweets(in_file1)
+	nb_data_2, nb_type_2, ref_list_2, coor_2, weight_2 = pre_info_insta(in_file2)
+
+	new_coor, new_weight, new_ref_list, new_nb_data = merge_datasets(ref_list_1,ref_list_2, nb_data_1,nb_data_2,nb_type_1, nb_type_2, coor_1, coor_2, weight_1, weight_2)
+
+	new_coor, new_weight = remove_duplication_coor(new_coor,new_weight,new_ref_list)
+
+	# Categories
+	cat_list, weight_category, corres_list = add_category(new_weight,new_ref_list,cat_file)
+
+	## Cluster by categories:
+
+
+	# 2 to test
+	new_weight, list_centroid, new_nb_data, list_coor = grid_creation(new_coor, weight_category)
+	new_weight = remove_outliers(list_centroid, new_weight, cat_list)
+	#return_cluster_pysal(list_centroid,new_weight, cat_list, new_nb_data,list_coor, out_file, out_file_2)
+	cluster_with_clusterpy(list_centroid,new_weight, cat_list, new_nb_data,list_coor, out_file, out_file_2)
+
+
+
+def show_cat_term(category, cat_file, out_file, out_file_2):
+	print "show terms"
+	nb_data_1, nb_type_1, ref_list_1, coor_1, weight_1 = pre_info_tweets(in_file1)
+	nb_data_2, nb_type_2, ref_list_2, coor_2, weight_2 = pre_info_insta(in_file2)
+
+	new_coor, new_weight, new_ref_list, new_nb_data = merge_datasets(ref_list_1,ref_list_2, nb_data_1,nb_data_2,nb_type_1, nb_type_2, coor_1, coor_2, weight_1, weight_2)
+	cat_list, weight_category, corres_list = add_category(new_weight,new_ref_list,cat_file)
+	new_weight, term_list = extract_cat(category, new_weight, new_ref_list, corres_list)
+	print term_list
+	new_coor, new_weight = remove_duplication_coor(new_coor,new_weight,term_list)
+	
+	new_weight, list_centroid, new_nb_data, list_coor = grid_creation(new_coor, new_weight)
+	new_weight = remove_outliers(list_centroid, new_weight, term_list)
+	new_nb_data = new_weight.shape[0]
+	# Create the clusters inside the category
+	#return_cluster_pysal(list_centroid,new_weight, term_list, new_nb_data,list_coor, out_file, out_file_2)
+	cluster_with_clusterpy(list_centroid,new_weight, term_list, new_nb_data,list_coor, out_file, out_file_2)
+	
+def show_cat_heatmap(category, cat_file, out_file, out_file_2):
+	nb_data_1, nb_type_1, ref_list_1, coor_1, weight_1 = pre_info_tweets(in_file1)
+	nb_data_2, nb_type_2, ref_list_2, coor_2, weight_2 = pre_info_insta(in_file2)
+
+	new_coor, new_weight, new_ref_list, new_nb_data = merge_datasets(ref_list_1,ref_list_2, nb_data_1,nb_data_2,nb_type_1, nb_type_2, coor_1, coor_2, weight_1, weight_2)
+	cat_list, weight_category, corres_list = add_category(new_weight,new_ref_list,cat_file)
+
+	unique_cat(new_coor, weight_category ,cat_list,category, out_file, out_file_2)
+
+
+if category == 'all':
+	show_all_cat(cat_file, out_file, out_file_2)
+else:
+	print bool_heatmap
+	if bool_heatmap == 'True':
+		show_cat_heatmap(category,cat_file, out_file, out_file_2)
+	else:
+		show_cat_term(category,cat_file, out_file, out_file_2)	
+
