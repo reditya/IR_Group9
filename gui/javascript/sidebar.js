6c9c651ca5c23f3be75aeff6631a671fc4889b18
Septian Gilang Permana Putra <septiangilang92@gmail.com>
2017-04-11 17:07:30 +0200

    Update clustering evaluation

--- gui/javascript/sidebar.js
+++ gui/javascript/sidebar.js
@@ -95,14 +95,11 @@ $.getJSON("category.json", function(data){
 
 
 $('#buttonSearch').on('click', function(e) {
-  if(global_radio_selection == "foodterm")
-  {
+  if(global_radio_selection == "foodterm"){
     searchPoints(0,23);
-  }
-  else
-  {
+  }else if (global_radio_selection == "category"){
     searchCategory(0,23);
-  }
+  }else{}
 });
 
 function searchPoints(start, end){
