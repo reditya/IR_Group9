33bcd747c0e3fe2e015f386b8a0741aa460f0b53
PHP server <reditya@gmail.com>
2017-04-11 06:42:47 +0000

    add foodterm and category selection

--- gui/javascript/sidebar.js
+++ gui/javascript/sidebar.js
@@ -28,6 +28,33 @@ $.getJSON("Amsterdam_AL8.GeoJson",function(data){
   L.geoJSON(data, geojsonMarkerOptions).addTo(map);
 });
 
+// add initial clustering
+$.getJSON("initial_clustering.geojson",function(data){
+  var geojsonMarkerOptions = {
+      fillOpacity: 1
+  };
+
+  //L.geoJSON(data, geojsonMarkerOptions).addTo(map);
+});
+
+// ACTION FOR FOOD TERM OR CATEGORY SEARCH
+var global_radio_selection = "";
+$('#radio_search input').on('change', function() {
+   var selected = $('input[name=optradio]:checked', '#radio_search').val();
+   global_radio_selection = selected; 
+   if(selected == "category")
+   {
+    $("#category-panel-form").show();
+    $("#foodterm-panel-form").hide();
+   }
+   else if(selected == "foodterm")
+   {
+    $("#foodterm-panel-form").show();
+    $("#category-panel-form").hide();
+   }
+});
+
+
 var pointMarker = new Array();
 
 var sidebar = L.control.sidebar('sidebar').addTo(map);
@@ -44,13 +71,38 @@ $.getJSON("food.json", function(data){
     selectHtml += "<option value='" + data[i]['name'] + "'" + ">" + data[i]['name'] + "</option>";
   }
   selectHtml += "</select>";
-  $("#select-panel-form").append(selectHtml);
+  $("#foodterm-panel-form").append(selectHtml);
   $("#foodSelection").chosen();
+  $("#foodterm-panel-form").hide();
+});
+
+var selectCategory = '';
+selectCategory += "<select id=categorySelection style='width:350px;'>";
+selectCategory += "<option value=''></option>";
+// chosen select panel
+
+$.getJSON("category.json", function(data){
+  for(i=0; i<data.length; i++)
+  {
+    console.log(data[i]);
+    selectCategory += "<option value='" + data[i]['name'] + "'" + ">" + data[i]['name'] + "</option>";
+  }
+  selectCategory += "</select>";
+  $("#category-panel-form").append(selectCategory);
+  $("#categorySelection").chosen();
+  $("#category-panel-form").hide();
 });
 
 
 $('#buttonSearch').on('click', function(e) {
-  searchPoints(0,23);
+  if(global_radio_selection == "foodterm")
+  {
+    searchPoints(0,23);
+  }
+  else
+  {
+    searchCategory(0,23);
+  }
 });
 
 function searchPoints(start, end){
@@ -108,6 +160,88 @@ function searchPoints(start, end){
   $.get(detail_query,function(data){
     $('#modalCollection').html('');
     $('#modalCollection').html(data);
+    alert(foodterm);
+    console.log(data);
+    //console.log(data);
+    var recipes_query = 'query.php?query=recipes&food='+foodterm+'&start='+start+'&end='+end;
+    $.getJSON(recipes_query,function(data){
+      //var recipes_html = '<h3>Top ' + foodterm + ' Recipes for you</h3><br>';
+      var recipes_html = '<p>';
+      //recipes_html = recipes_html + '<ul>';
+      for(var i=0; i < data.length; i++)
+      {
+        recipes_html = recipes_html 
+          + "<div class='card w-100'>" +
+              "<div class='card-block'>" + 
+                "<h4 class='card-title'>" + data[i]['title'] + "</h4>" + 
+                "<p class='card-text'>" + data[i]['description'] + "</p>" + 
+                "<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#recipeModal" + i + "'>Detail</button>" +  
+              "</div>" + 
+            "</div><br>";          
+        //console.log(recipes_html);
+        //recipes_html = recipes_html + '<li>' + data[i]['title'] + '</li>';
+      }
+      recipes_html = recipes_html + '</p>';
+      //recipes_html = recipes_html + '</ul>';
+      $("#span_recipes").html('');
+      $("#span_recipes").html(recipes_html);        
+    });
+  });
+}
+
+function searchCategory(start, end){
+  for(i=0; i<pointMarker.length; i++)
+  {
+    map.removeLayer(pointMarker[i]);
+  }
+
+  //var foodterm = $("#countries").getSelectedItemData().name;
+  var foodterm = $("#categorySelection").val();  
+  foodterm = foodterm.toLowerCase();
+
+  var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  };
+
+  //alert(foodterm);
+
+  // this needs to be changed
+  var food_query = 'query.php?query=category&food='+foodterm+'&start='+start+'&end='+end;
+  $.getJSON(food_query,function(data){
+    var pointData = data['points'];
+    var clusterData = data['clusters'];
+    console.log(pointData);
+    marker_point = L.geoJSON(pointData);
+    //marker_cluster = L.geoJSON(clusterData);
+
+    var data_heat = [];
+
+    var datapoint = pointData['features'];
+    for(i=0;i<datapoint.length;i++)
+    {
+      //console.log(datapoint[i]);
+      data_heat.push([datapoint[i]['geometry']['coordinates'][1], datapoint[i]['geometry']['coordinates'][0], datapoint[i]['properties']['intensity']]);
+    }
+
+    //console.log(data_heat);
+
+    var heat_layer = L.heatLayer(data_heat, {radius: 15, maxZoom: 15});
+    pointMarker.push(heat_layer);
+    map.addLayer(heat_layer);
+  });
+
+  // recipes detail in the sidebar
+  var detail_query = 'query.php?query=recipesDetail&food='+foodterm+'&start='+start+'&end='+end;
+  $.get(detail_query,function(data){
+    $('#modalCollection').html('');
+    $('#modalCollection').html(data);
+    alert(foodterm);
+    console.log(data);
     //console.log(data);
     var recipes_query = 'query.php?query=recipes&food='+foodterm+'&start='+start+'&end='+end;
     $.getJSON(recipes_query,function(data){
